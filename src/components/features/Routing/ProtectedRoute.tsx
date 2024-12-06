ab465f3151f85a5a8d568244cee88f1422c2a162
Paweł Kozłowski <p.awel608@wp.pl>
2024-12-06 18:29:51 +0100

    useNavigate used instead of Navigate

--- src/components/features/Routing/ProtectedRoute.tsx
+++ src/components/features/Routing/ProtectedRoute.tsx
@@ -1,27 +1,36 @@
-import {useContext} from "react";
-import {AuthContext} from "@context/AuthContext/AuthContext.ts";
-import {Navigate, Outlet, useLocation} from "react-router-dom";
-import {ILoggedUserData} from "@shared/userTypes.ts";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "@context/AuthContext/AuthContext.ts";
+import { Outlet, useNavigate } from "react-router-dom";
+import { ILoggedUserData } from "@shared/userTypes.ts";
 
 const ProtectedRoute = () => {
-	const {user} = useContext(AuthContext);
-	const {pathname} = useLocation();
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-	const isProfileCompleted = (user: ILoggedUserData) => {
-		return !Object.entries(user)
-			.filter(([key]) => key !== 'crm')
-			.map(([, value]) => value)
-			.some(value => value === null || (Array.isArray(value) && value.length === 0));
-	};
-	if (!user) {
-    return <Navigate to="/login" replace />;
-  }
-   if (!isProfileCompleted(user) && pathname !== '/onboarding') {
-    return <Navigate to="/onboarding" replace />;
-  }
+  useEffect(() => {
+    if (!user) {
+      navigate("/login");
+      return;
+    }
+    if (!isProfileCompleted(user)) {
+      navigate("/onboarding");
+      return;
+    }
+  }, [navigate, user]);
 
-	return <Outlet />;
+  const isProfileCompleted = (user: ILoggedUserData) => {
+    return !Object.entries(user)
+      .filter(([key]) => key !== "crm")
+      .map(([, value]) => value)
+      .some(
+        (value) =>
+          value === null || (Array.isArray(value) && value.length === 0)
+      );
+  };
+
+  if (!user) return null;
+
+  return <Outlet />;
 };
 
 export default ProtectedRoute;
-
