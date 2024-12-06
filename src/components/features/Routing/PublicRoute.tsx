b89a5017972912ceadd58c414c2d0ba8b82b6671
Paweł Kozłowski <p.awel608@wp.pl>
2024-12-06 18:30:33 +0100

    useNavigate on public route

--- src/components/features/Routing/PublicRoute.tsx
+++ src/components/features/Routing/PublicRoute.tsx
@@ -1,15 +1,19 @@
-import {useContext} from "react";
+import {useContext, useEffect} from "react";
 import {AuthContext} from "@context/AuthContext/AuthContext.ts";
-import {Navigate, Outlet} from "react-router-dom";
+import {Outlet, useNavigate} from "react-router-dom";
 
 const PublicRoute = () => {
-	const { user } = useContext(AuthContext);
-  
-	if (user) {
-	  return <Navigate to="/" replace />;
-	}
-  
-	return <Outlet />;
-  };
+	const {user} = useContext(AuthContext);
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (user) {
+			navigate("/");
+			return;
+		}
+	}, [navigate, user]);
+
+	return <Outlet/>
+}
 
 export default PublicRoute;
