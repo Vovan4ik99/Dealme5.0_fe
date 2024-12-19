24af65c0dd118dcead86aa55ec2a48b923b962c6
Wlodek-YouCanDev <sukhina.vova@gmail.com>
2024-12-19 15:22:59 +0100

    code refactoring

--- src/components/features/Routing/ProtectedRoute.tsx
+++ src/components/features/Routing/ProtectedRoute.tsx
@@ -1,50 +1,50 @@
-import { useContext, useEffect } from "react";
-import { AuthContext } from "@context/AuthContext/AuthContext.ts";
-import { Outlet, useNavigate } from "react-router-dom";
-import { ILoggedUserData } from "@shared/userTypes.ts";
+import {useContext, useEffect} from "react";
+import {AuthContext} from "@context/AuthContext/AuthContext.ts";
+import {Outlet, useNavigate} from "react-router-dom";
+import {ILoggedUserData} from "@shared/userTypes.ts";
 
 const ProtectedRoute = () => {
-  const { user } = useContext(AuthContext);
-  const navigate = useNavigate();
+	const {user} = useContext(AuthContext);
+	const navigate = useNavigate();
 
-  useEffect(() => {
-    if (!user) {
-      navigate("/login");
-      return;
-    }
-    if (!isProfileCompleted(user)) {
-      navigate("/onboarding");
-    }
-  }, [navigate, user]);
+	useEffect(() => {
+		if (!user) {
+			navigate("/login");
+			return;
+		}
+		if (!isProfileCompleted(user)) {
+			navigate("/onboarding");
+		}
+	}, [navigate, user]);
 
-  const isProfileCompleted = (user: ILoggedUserData) => {
-    const requiredFields = [
-      "email",
-      "experienceLevel",
-      "firstName",
-      "id",
-      "lastName",
-      "role",
-      "incomeGoal",
-      "salesTools",
-      "sectors",
-      "selectedActivities",
-      "specialization",
-      "subIndustries",
-      "typeOfSales",
-      "workingDays",
-      "workingHours",
-    ];
-    return !Object.entries(user)
-      .filter(([key]) => requiredFields.includes(key))
-      .map(([, value]) => value)
-      .some(
-        (value) =>
-          value === null || (Array.isArray(value) && value.length === 0)
-      );
-  };
+	const isProfileCompleted = (user: ILoggedUserData) => {
+		const requiredFields = [
+			"email",
+			"experienceLevel",
+			"firstName",
+			"id",
+			"lastName",
+			"role",
+			"incomeGoal",
+			"salesTools",
+			"sectors",
+			"selectedActivities",
+			"specialization",
+			"subIndustries",
+			"typeOfSales",
+			"workingDays",
+			"workingHours",
+		];
+		return !Object.entries(user)
+			.filter(([key]) => requiredFields.includes(key))
+			.map(([, value]) => value)
+			.some(
+				(value) =>
+					value === null || (Array.isArray(value) && value.length === 0)
+			);
+	};
 
-  return <Outlet />;
+	return <Outlet/>;
 };
 
 export default ProtectedRoute
