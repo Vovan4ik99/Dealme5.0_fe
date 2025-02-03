def8b3dc074720ae56d098fdfcb8d0b6a5b000eb
Wlodek-YouCanDev <sukhina.vova@gmail.com>
2025-02-03 20:23:13 +0100

    fix build error

--- src/components/features/EditModal/media/VideoPreviewer/VideoPreviewer.tsx
+++ src/components/features/EditModal/media/VideoPreviewer/VideoPreviewer.tsx
@@ -2,7 +2,7 @@ import React, { useCallback, useEffect } from "react";
 import { IVideoPreviewerProps } from "@components/features/EditModal/media/VideoPreviewer/videoPreviewerTypes.ts";
 import { useModal } from "@context/ModalContext/ModalContext.ts";
 import VideoItem from "@ui/VideoItem/VideoItem.tsx";
-import styles from './videoPreviewer.module.scss';
+import styles from './VideoPreviewer.module.scss';
 
 const VideoPreviewer: React.FC<IVideoPreviewerProps> = ({ onClose, registerOnSave, videoUrl }) => {
 
