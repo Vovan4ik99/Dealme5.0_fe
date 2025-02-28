dd81fe4b5fbb2eb377622e528a3b10d631d281c8
K0nradSobczak <konradsobczak14@gmail.com>
2025-02-28 10:55:05 +0100

    Correct reset password pr (#25)
    * version on correct branch
    
    * simplify code
    
    * fixing scss and removing clutter states
    
    * delete unnecessary spaces and braces changing class name on reset-page

---
+++ src/pages/Auth/ResetPasswordPage/ResetPasswordPage.tsx
@@ -0,0 +1,19 @@
+
+import styles from "./ResetPasswordPage.module.scss"
+import Footer from "@components/layout/Footer/Footer.tsx";
+import React from "react";
+import ResetPasswordForm from "@components/features/Auth/ResetPasswordForm/ResetPasswordForm.tsx";
+import Navbar from "@components/layout/OnboardingNavbar/Navbar.tsx";
+
+
+const ResetPasswordPage = () => {
+    return (
+        <section className={styles['reset-page']}>
+            <Navbar/>
+            <ResetPasswordForm/>
+            <Footer isHyphenated={true} isCentered={true}/>
+        </section>
+    )
+}
+
+export default ResetPasswordPage;
