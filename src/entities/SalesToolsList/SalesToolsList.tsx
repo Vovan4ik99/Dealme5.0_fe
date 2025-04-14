a1a7c4b19b9d99b0db33688793eecd18d8e0e664
Volodymyr Sukhina <75521182+Vovan4ik99@users.noreply.github.com>
2025-04-14 14:10:57 +0200

    Fix freelancer onboarding bugs (#54)
    * Fix freelancer onboarding bugs (#53)

--- src/entities/SalesToolsList/SalesToolsList.tsx
+++ src/entities/SalesToolsList/SalesToolsList.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ISalesToolsListProps } from "@entities/SalesToolsList/salesToolsListTypes.ts";
 import OnboardingSearchBar from "@components/features/onboarding/OnboardingSearchBar/OnboardingSearchBar.tsx";
 import { getToolKindNameByKind } from "@utils/salesToolsUtils.ts";
@@ -9,7 +9,11 @@ import OnboardingCategoryItem from "@ui/onboarding/OnboardingCategoryItem/Onboar
 
 const SalesToolsList: React.FC<ISalesToolsListProps> = ({ tools, selectedTools, setSelectedTools }) => {
 
-	const [ filteredTools, setFilteredTools ] = useState<ISalesTool[]>(tools);
+	const [ filteredTools, setFilteredTools ] = useState<ISalesTool[]>([]);
+
+	useEffect(() => {
+		setFilteredTools(tools);
+	}, [ tools ]);
 
 	const handleToolClick = (tool: ISalesTool) => {
 		setSelectedTools(prevState => {
@@ -20,32 +24,33 @@ const SalesToolsList: React.FC<ISalesToolsListProps> = ({ tools, selectedTools, 
 		});
 	};
 
+	const renderCategory = (category: string) => {
+		const toolsInCategory = filteredTools
+			.filter(tool => tool.kind === category);
+
+		const categoryContent = toolsInCategory.map(tool => {
+			const isActive = selectedTools.map(item => item.id).includes(tool.id);
+			return <OnboardingOption key={ tool.id }
+			                         title={ tool.toolName }
+			                         titleFontSize={ 16 }
+			                         isActive={ isActive }
+			                         withCheckboxInput
+			                         image={ getPictureForSalesTools(tool.toolName) }
+			                         onClick={ () => handleToolClick(tool) }/>
+		});
+
+		const isActive = categoryContent.some(content => content.props.isActive);
+
+		return <OnboardingCategoryItem key={ category }
+		                               text={ getToolKindNameByKind(category) }
+		                               isActive={ isActive }
+		                               categoryContent={ categoryContent }/>
+	};
+
 	const renderContent = () => {
 		return Array.from(
 			new Set(filteredTools.map(tool => tool.kind))
-		).map(category => {
-				const toolsInCategory = filteredTools
-					.filter(tool => tool.kind === category);
-
-				const categoryContent = toolsInCategory.map(tool => {
-					const isActive = selectedTools.map(item => item.id).includes(tool.id);
-					return <OnboardingOption key={ tool.id }
-					                         title={ tool.toolName }
-					                         titleFontSize={ 16 }
-					                         isActive={ isActive }
-					                         withCheckboxInput
-					                         image={ getPictureForSalesTools(tool.toolName) }
-					                         onClick={ () => handleToolClick(tool) }/>
-				});
-
-				const isActive = categoryContent.some(content => content.props.isActive);
-
-				return <OnboardingCategoryItem key={ category }
-				                               text={ getToolKindNameByKind(category) }
-				                               isActive={ isActive }
-				                               categoryContent={ categoryContent }/>
-			}
-		);
+		).map(category => renderCategory(category));
 	};
 
 	const onSearch = (searchStr: string) => {
